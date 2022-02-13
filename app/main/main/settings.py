d0b4ae67c783a4ce835e14d04679597c8f134281
losthunter52 <maherdaoud52@gmail.com>
2022-02-12 21:55:19 -0300

    adicionado verificação de grupos de usuario

--- app/main/main/settings.py
+++ app/main/main/settings.py
@@ -70,6 +70,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+            'user_tags': 'core.templatetags.user_tags',
+            
+            }
         },
     },
 ]
