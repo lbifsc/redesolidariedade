9aeb9f1b77d179f8104235f36f7010956d04c723
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2022-06-26 22:01:51 -0300

    Relatório - Família

--- app/main/core/views.py
+++ app/main/core/views.py
@@ -462,6 +462,19 @@ class listaFamilia(LoginRequiredMixin, ListView):
             )
         return queryset
 
+class listaFamiliaRelatorio(LoginRequiredMixin, ListView):
+    model = Familia
+    template_name = 'listaFamiliaRelatorio.html'
+    context_object_name = 'familias'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 class listaRepresentante(LoginRequiredMixin, ListView):
     model = Representante
     template_name = 'listaRepresentante.html'
